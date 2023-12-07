77faedbe46755747a4e43e6442a5c26a5cee5242
Rafael Silva <devrafasilva@gmail.com>
2023-12-07 16:08:12 -0300

    Projeto com Surf

--- codigo/script-events-items/carrinhodeCompras.js
+++ codigo/script-events-items/carrinhodeCompras.js
@@ -1,12 +1,13 @@
 export default function carrinhoEvent() {
-  const produtoEvent = document.querySelector(".conteudo-item-produtos");
+  const produtoEvent = document.querySelector("[data-carrinho-item]");
   const addProduto = document.querySelectorAll("[data-toolti-event]");
+  const carrinhBlock = document.querySelector(".carrinho-section");
+  const containerCarrinhoItem = document.querySelectorAll(".item-carrinho");
 
   function addEvent(index) {
-    console.log(produtoEvent.innerText[index]);
+    console.log((produtoEvent.innerHTML = containerCarrinhoItem));
+    carrinhBlock.classList.add("on");
   }
-  git;
-  g;
 
   addProduto.forEach((itemclick, index) => {
     itemclick.addEventListener("click", () => {
